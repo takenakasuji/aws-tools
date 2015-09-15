f67523f7c8486acb1ee8ca9b2ad0133ea8c3a71b
takenakasuji <nakasuji@nakasuji-no-MacBook-Pro.local>
2015-09-15 22:11:54 +0900

    add create IGW

--- create_vpc.sh
+++ create_vpc.sh
@@ -1,5 +1,6 @@
+#==========Create VPC base part==========
 # Declare Environment Variable 
-export CIDR=10.100.0.0/16
+export CIDR=10.0.0.0/16
 export VPCNAME=MyVPC
 
 # Create VPC
@@ -16,3 +17,23 @@ aws ec2 modify-vpc-attribute --vpc-id $VPCID --enable-dns-hostnames
 aws ec2 describe-vpcs --vpc-ids $VPCID
 aws ec2 describe-vpc-attribute --vpc-id $VPCID --attribute enableDnsSupport
 aws ec2 describe-vpc-attribute --vpc-id $VPCID --attribute enableDnsHostnames
+
+read -p "Press [Enter] key to resume."
+
+#==========Create IGW part==========
+# Declare Environment Variable 
+export IGWNAME=MyIGW
+
+# Create Internet Gateway
+export IGWID=$(aws ec2 create-internet-gateway | awk '/InternetGatewayId/ {gsub(/\"/, "");gsub(/,/,""); print $2}')
+
+# Configure IGW Name Tag
+aws ec2 create-tags --resources $IGWID --tags Key=Name,Value=$IGWNAME
+
+# Allocate to VPC
+aws ec2 attach-internet-gateway --internet-gateway-id $IGWID --vpc-id $VPCID
+
+# Confirm
+aws ec2 describe-internet-gateways --filters "Name=internet-gateway-id,Values=$IGWID"
+
+read -p "Press [Enter] key to resume."
