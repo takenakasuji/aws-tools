a4efe4d74b4f1d68a6dc36640130d78330ae8362
takenakasuii <nakasuji@nakasuji-no-MacBook-Pro.local>
2015-09-17 00:15:25 +0900

    add
    add a Ingress rule

--- create_security_group.sh
+++ create_security_group.sh
@@ -2,6 +2,12 @@
 export VPCID=<input vpc-id>
 export GROUPNAME=<input group name>
 export DESCRIPTION=<input description>
+export SSH=<input ssh port number>
+export HTTP=<input HTTP port number>
 
+# Create Security Group
 export GROUPID=$(aws ec2 create-security-group --group-name $GROUPNAME --description $DESCRIPTION --vpc-id $VPCID | awk '/GroupId/ {gsub(/\"/, "");gsub(/,/,""); print $2}')
 
+# Add Ingress Rule
+aws ec2 authorize-security-group-ingress --group-id $GROUPID --protocol tcp --port $SSH --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $GROUPID --protocol tcp --port $HTTP --cidr 0.0.0.0/0
